3b61214f91cf1f28a1cfa37689c84eab2d7570e3
DominicDev <s0l0estudi4r@gmail.com>
2025-09-25 20:11:17 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'GUNS N’ ROSES';
+export const nameSongs = [
+	"WELCOME TO THE JUNGLE", /*1*/
+	"SWEET CHILD O' MINE", /*2*/
+	"PARADISE CITY", /*3*/
+	"KNOCKIN' ON HEAVEN'S DOOR", /*4*/
+	"DON'T CRY", /*5*/
+	"IT'S SO EASY ", /*6*/
+	"NOVEMBER RAIN", /*7*/
+	"YOU COULD BE MINE ", /*8*/
+	"CIVIL WAR", /*9*/
+	"ESTRANGED ", /*10*/
+	"LIVE AND LET DIE ", /*11*/
+	"NIGHTRAIN", /*12*/
+	"YESTERDAYS ", /*13*/
+	" MR. BROWNSTONE ", /*14*/
+	"ROCKET QUEEN", /*15*/
+	" MY MICHELLE ", /*16*/
+	"PATIENCE ", /*17*/
+	"BACK OFF BITCH ", /*18*/
+	"YOU'RE CRAZY ", /*19*/
+	"GARDEN OF EDEN ", /*20*/
+]
